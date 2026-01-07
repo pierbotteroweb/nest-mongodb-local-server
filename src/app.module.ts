b5712a2fdb385cf0c52c72752260d83033c06562
Pier Bottero <pierbotteroweb@gmail.com>
2026-01-07 15:13:27 -0300

    controler and api

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongoModule } from './database/mongo/mongo.module';
+import { TestController } from './test/test.controller';
 
 @Module({
   imports: [MongoModule],
-  controllers: [AppController],
+  controllers: [AppController, TestController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
 
   const logger = new Logger('Bootstrap');
 
