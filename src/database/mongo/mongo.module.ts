b75168fd20a5f155ce0efcc6357131f91c99000e
Pier Bottero <pierbotteroweb@gmail.com>
2026-01-07 04:37:07 -0300

    mongodb connection

---
+++ src/database/mongo/mongo.module.ts
@@ -0,0 +1,33 @@
+import { Logger, Module, OnModuleInit } from '@nestjs/common';
+import { InjectConnection, MongooseModule } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://localhost:27017/shufleTV?replicaSet=rs0',
+      { autoIndex: true },
+    ),
+  ],
+})
+export class MongoModule implements OnModuleInit {
+  private readonly logger = new Logger('MongoDB');
+
+  constructor(
+    @InjectConnection() private readonly connection: Connection,
+  ) {}
+
+  onModuleInit() {
+    if (this.connection.readyState === 1) {
+      this.logger.log('🟢 Connected to ShuffleTV Database 1');
+    }
+
+    this.connection.on('connected', () => {
+      this.logger.log('🟢 Connected to ShuffleTV Database 2');
+    });
+
+    this.connection.on('error', (err) => {
+      this.logger.error('🔴 Error connecting to MongoDB', err);
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 
-  console.log('🚀 Servidor rodando em http://localhost:3000');
+  const logger = new Logger('Bootstrap');
+
+  logger.log('🚀 Servidor rodando em http://localhost:3000');
 }
 bootstrap();
