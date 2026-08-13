38052326f50d83a906a5c86f164beec1272d3874
Pier Bottero <pierbotteroweb@gmail.com>
2026-08-13 00:58:17 -0300

    cors desabilitado apos nginx

--- src/main.ts
+++ src/main.ts
@@ -2,42 +2,42 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
-const defaultCorsOrigins = [
-  'http://localhost',
-  'http://localhost:4200',
-  'http://localhost:4444',
-  'http://localhost:1991',
-  'http://127.0.0.1',
-  'http://127.0.0.1:4200',
-  'http://127.0.0.1:4444',
-  'http://thisisshuffletv',
-  'http://thisisshuffletv.local',
-  'http://thisisshuffletv:4444',
-  'http://thisisshuffletv.local:4444',
-];
+// const defaultCorsOrigins = [
+//   'http://localhost',
+//   'http://localhost:4200',
+//   'http://localhost:4444',
+//   'http://localhost:1991',
+//   'http://127.0.0.1',
+//   'http://127.0.0.1:4200',
+//   'http://127.0.0.1:4444',
+//   'http://thisisshuffletv',
+//   'http://thisisshuffletv.local',
+//   'http://thisisshuffletv:4444',
+//   'http://thisisshuffletv.local:4444',
+// ];
 
-const allowedCorsOrigins = new Set(
-  (process.env.CORS_ORIGINS ?? defaultCorsOrigins.join(','))
-    .split(',')
-    .map((origin) => origin.trim())
-    .filter(Boolean),
-);
+// const allowedCorsOrigins = new Set(
+//   (process.env.CORS_ORIGINS ?? defaultCorsOrigins.join(','))
+//     .split(',')
+//     .map((origin) => origin.trim())
+//     .filter(Boolean),
+// );
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || allowedCorsOrigins.has(origin)) {
-        callback(null, true);
-        return;
-      }
+  // app.enableCors({
+  //   origin: (origin, callback) => {
+  //     if (!origin || allowedCorsOrigins.has(origin)) {
+  //       callback(null, true);
+  //       return;
+  //     }
 
-      callback(null, false);
-    },
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
+  //     callback(null, false);
+  //   },
+  //   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  // });
 
 
   const logger = new Logger('Bootstrap');
